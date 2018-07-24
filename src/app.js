3851b21f07fe295aac98072a9c1dfdf5ef52c7ad
Dam SamNang <dam.sam.nang@framgia.com>
2018-07-24 09:39:14 +0700

    react and redux

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill'}));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const visableExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(store.getState());
+console.log(visableExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
