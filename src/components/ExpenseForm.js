5013cd569eef0dec59a8b3242e6956d0f3fa8938
Dam SamNang <dam.sam.nang@framgia.com>
2018-07-25 09:57:27 +0700

    form amount note description

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.Component {
+  state = {
+    description: '',
+    note: '',
+    amount: ''
+  };
+
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  };
+
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
+  };
+
+  onAmountChange = (e) => {
+    const amount = e.target.value;
+    if (amount.match(/^\d*(\.\d{0,2})?$/) ) {
+      this.setState(() => ({ amount }));
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <input
+            type="text"
+            placeholder="Amount"
+            onChange={this.onAmountChange}
+          />
+          <textarea
+            value={this.state.note}
+            placeholder="Add a note for any optional"
+            onChange={this.onNoteChange}
+          >
+          </textarea>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    )
+  }
+}
