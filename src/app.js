80a6085803a91e465172dacead4a8e7c293d8d47
Dam SamNang <dam.sam.nang@framgia.com>
2018-08-22 14:12:53 +0700

    filter date

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 1000 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 3000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 211000 }));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visableExpenses = getVisibleExpenses(state.expenses, state.filters);
