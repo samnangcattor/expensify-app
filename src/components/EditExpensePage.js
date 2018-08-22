38e4f33156e552207ae433ab834670641c171c37
Dam SamNang <dam.sam.nang@framgia.com>
2018-08-22 11:09:35 +0700

    add remove edit expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import  ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
+import { Redirect } from 'react-router-dom';
 
-const EditExpensePage = (props) => {
-  console.log(props);
+const EditExpensePage = ({ expense, dispatch, history }) => {
   return (
   <div>
-    This is from my edit expense component.
+    <ExpenseForm
+      expense={expense}
+      onSubmit={(update) => {
+        dispatch(editExpense(expense.id, update));
+        history.push('/');
+      }}
+    />
+    <button onClick={() => {
+      dispatch(removeExpense({ id: expense.id }));
+      history.push('/');
+    }}>
+      Remove
+    </button>
   </div>
 )};
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
